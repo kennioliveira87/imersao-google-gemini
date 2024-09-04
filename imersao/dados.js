cee35d3316636204ba5bf73af2eaa331dd6ac692
Kenni Oliveira <kennioliveira2@outlook.com>
2024-09-04 14:35:03 -0300

    atualizado

---
+++ imersao/dados.js
@@ -0,0 +1,29 @@
+let dados = [
+    {
+        titulo : "Metallica", 
+        descricao : " O Metallica, formado em 1981, é uma das bandas de heavy metal mais influentes e icônicas de todos os tempos. Com uma sonoridade distintiva e performances enérgicas, a banda conquistou fãs ao redor do mundo e deixou um legado duradouro na história do rock, influenciando inúmeras bandas e músicos.",
+        link : "https://www.metallica.com/"
+    },
+    {
+        titulo : "System of a Down",
+        descricao : "System of a Down é uma banda de metal alternativo formada em 1995. Conhecidos por suas letras politicamente engajadas e mistura de estilos musicais, como heavy metal, rock alternativo e música tradicional armênia, a banda conquistou uma legião de fãs ao redor do mundo.",
+        link : "https://www.systemofadown.com/"
+    },
+    {
+        titulo : "Slipknot",
+        descrucao : "Slipknot é uma banda de metal alternativo formada em 1995. Conhecidos por suas máscaras, macacões e apresentações intensas, a banda mescla elementos do nu metal, groove metal e heavy metal, criando um som agressivo e único.",
+        link : "https://www.slipknot1.com/"
+    },
+    {
+        titulo : "Sepultura",
+        descricao : "Formada em Belo Horizonte, Minas Gerais, em 1984, o Sepultura é uma das bandas de metal mais importantes do Brasil e uma das pioneiras do thrash metal na América Latina. Com uma sonoridade agressiva e letras engajadas, o Sepultura conquistou fãs em todo o mundo e se tornou um dos maiores expoentes do metal brasileiro.",
+        link : "https://www.sepultura.com.br/" 
+    },
+    {
+        titulo : "Raimundos",
+        descricao : "Os Raimundos, formados em Brasília em 1987, são uma das bandas de rock mais populares do Brasil. Com um som que mistura elementos do punk rock, hardcore e música brasileira, os Raimundos conquistaram um grande público e se tornaram um dos maiores ícones do rock nacional.",
+        link : "https://www.raimundos.com.br/"
+    },
+    
+];
+
