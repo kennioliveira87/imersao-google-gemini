f7aa4185973f75a99a6cfad11ffd284a943ea792
Kenni Oliveira <kennioliveira2@outlook.com>
2024-09-05 14:13:21 -0300

    atualizei

--- imersao/app.js
+++ imersao/app.js
@@ -1,4 +1,15 @@
-console.log(dados);
+//console.log(dados);
 
 
 let section = document.getElementById("resultados-pesquisa")
+console.log("section");
+
+section.innerHTML = `
+    <div class="item-resultado">
+        <h2>
+            <a href="#" target="_blank">Metallica</a>
+        </h2>
+        <p class="descricao-meta"></p>
+        <a href="https://www.metallica.com/" target="_blank">Acesse o Link:</a>
+    </div>
+`
