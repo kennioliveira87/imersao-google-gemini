0e19cc50dc3d10a1126b5569651502aa607d290f
Kenni Oliveira <kennioliveira2@outlook.com>
2024-09-06 11:02:01 -0300

    atualizado

--- imersao/app.js
+++ imersao/app.js
@@ -1,15 +1,29 @@
-//console.log(dados);
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+        // Constrói o HTML para cada item do resultado da pesquisa,
+        // formatando os dados do objeto 'dado'
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Acesse o Link:</a>
+            </div>
+        `;
+    }
+
+    // Atribui o HTML gerado para a seção de resultados
+    section.innerHTML = resultados;
+}
 
 
-let section = document.getElementById("resultados-pesquisa")
-console.log("section");
 
-section.innerHTML = `
-    <div class="item-resultado">
-        <h2>
-            <a href="#" target="_blank">Metallica</a>
-        </h2>
-        <p class="descricao-meta"></p>
-        <a href="https://www.metallica.com/" target="_blank">Acesse o Link:</a>
-    </div>
-`
+
